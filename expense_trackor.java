46f4a049836884dbb2ba2f84c8a51915a90ac1be
kaustavnandi08-code <kaustavnandi08@gmail.com>
2026-07-29 12:33:10 +0530

    Correct loan_expense declaration in expense_trackor
    Fix variable declaration and formatting in expense tracking logic.

--- expense_trackor.java
+++ expense_trackor.java
@@ -7,7 +7,7 @@ int salary=sc.nextInt();
 int expense=sc.nextInt();
  if(salary>10000 && salary<20000) {
         int savings=salary-expense;
-        int
+        int loan_expense=25000;
         System.out.println("You can save 20% of your salary");
     }
     
